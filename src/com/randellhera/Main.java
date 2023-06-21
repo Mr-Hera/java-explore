2f8778f0d6b32f985c1e22d01a3ed0aa2062dc01
Mr-Hera <randellhera@gmail.com>
2023-06-21 16:45:25 +0300

    Feat: 0 & null values for arrays

--- src/com/randellhera/Main.java
+++ src/com/randellhera/Main.java
@@ -7,21 +7,15 @@ import java.util.Arrays;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-//        Arrays
+//        Arrays null values
         int[] numbers = new int[3];
-        numbers[0] = 1;
-        numbers[1] = 33;
-        numbers[2] = 6;
+        Arrays.fill(numbers, -1);
+        numbers[2] = 27;
         System.out.println(Arrays.toString(numbers));
-        System.out.println(numbers.length);
 
-        int[] numbers2 = {0, 5, 6, 8, 9};
-        System.out.println(Arrays.toString(numbers2));
-        numbers2[1] = 55;
-        System.out.println(Arrays.toString(numbers2));
-        System.out.println(numbers2.length);
-
-        String[] people = {"Randell", "Hera"};
-        System.out.println(Arrays.toString(people));
+        String[] names = new String[3];
+        Arrays.fill(names, "foo");
+        names[1] = "Jamila";
+        System.out.println(Arrays.toString(names));
     }
 }
