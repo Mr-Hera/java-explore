04c9c2821e97ca1239a474011127423dd9b20aab
Mr-Hera <randellhera@gmail.com>
2023-06-21 17:53:03 +0300

    Feat: sample method

--- src/com/randellhera/Main.java
+++ src/com/randellhera/Main.java
@@ -12,7 +12,16 @@ public class Main {
         - Method Body
         - Optional Return Value
     */
+
+    private static int add(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
+    }
+
     public static void main(String[] args) {
-//        body
+        int result = add(10, 10);
+        int result2 = add(10, 100);
+        System.out.println(result);
+        System.out.println(result2);
     }
 }
