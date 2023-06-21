b9a3f412f168aaceb3a6ccc22baed0b65f4a776a
Mr-Hera <randellhera@gmail.com>
2023-06-21 16:23:46 +0300

    Feat: Referrence Types & Objects

--- src/com/randellhera/Main.java
+++ src/com/randellhera/Main.java
@@ -1,18 +1,18 @@
 package com.randellhera;
 
+import java.awt.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        String fName = "Randell";
-        String lName = "Hera";
-        String fullName = fName + lName;
-
-        System.out.println(fullName.toUpperCase());
-        System.out.println(fullName.toLowerCase());
-        System.out.println(fullName.substring(7));
-        System.out.println(" ".isEmpty());
-        System.out.println(" ".isBlank());
-        System.out.println(" a ".trim());
+//        Reference types / Objects
+        int age = 21;
+        Point pointA = new Point(10, 10);
+        Point pointB = new Point(20, 20);
+        String brand = "Cepic";
+        System.out.println("pointA = " + pointA);
+        pointA.move(10, 11);
+        System.out.println("pointA = " + pointA);
     }
 }
